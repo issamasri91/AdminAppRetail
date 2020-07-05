584ee02e85c15fab0c4dbaa2628e0f29f206018c
IssamElasri <elasriisssam@gmail.com>
2020-07-06 00:01:03 +0100

    up date Add stock

--- app/src/main/java/com/issamelasri/adminappretail/stock/StockVendeur.kt
+++ app/src/main/java/com/issamelasri/adminappretail/stock/StockVendeur.kt
@@ -1,15 +1,26 @@
 package com.issamelasri.adminappretail.stock
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.viewpager.widget.ViewPager
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.issamelasri.adminappretail.R
+import com.issamelasri.adminappretail.stock.models.StockAccessoir
+import com.issamelasri.adminappretail.stock.models.StockGlobal
 import com.issamelasri.adminappretail.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.enter_acc_stock.*
+import kotlinx.android.synthetic.main.fragment_stock_acc.*
 
 class StockVendeur : AppCompatActivity() {
-
+    private var stock: StockGlobal = StockGlobal()
+    private val model: ViewModelStock by viewModels()
+    private var pageActive = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock_vendeur)
@@ -19,9 +30,90 @@ class StockVendeur : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
 
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    when (tab.position) {
+                        0 -> pageActive = 0
+                        1 -> pageActive = 1
+                        2 -> pageActive = 2
+                        3 -> pageActive = 3
+                    }
+                }
+            }
+        })
         fab.setOnClickListener {
+            val dialog = MaterialDialog(this)
+                .noAutoDismiss()
+            dialog.customView(R.layout.enter_acc_stock)
+            //init
+            dialog.stock_accessoir_cable_editText.setText(stock_accessoir_cable.text)
+            dialog.stock_carte_memoir_editText.setText(stock_carte_memoir.text)
+            dialog.stock_chargeur.setText(char_stock_acc.text)
+            dialog.stock_ecuteur.setText(ecteur_stock.text)
+            dialog.stock_protection.setText(protection_stock.text)
+            dialog.stock_pochet.setText(puchet_stock.text)
+            dialog.stock_support.setText(supportStock.text)
+            dialog.stock_autre.setText(autre_acc_stock.text)
 
+            dialog.positiveButton {
+                when (pageActive) {
+                    0 -> {
+                        val cable = dialog.stock_accessoir_cable_editText.text
+                        stock_accessoir_cable.text = cable
+                        val carteMimoir = dialog.stock_carte_memoir_editText.text
+                        stock_carte_memoir.text = carteMimoir
+                        val chargeur = dialog.stock_chargeur.text
+                        char_stock_acc.text = chargeur
+                        val ecuteur = dialog.stock_ecuteur.text
+                        ecteur_stock.text = ecuteur
+                        val protection = dialog.stock_protection.text
+                        protection_stock.text = protection
+                        val pochet = dialog.stock_pochet.text
+                        puchet_stock.text = pochet
+                        val support = dialog.stock_support.text
+                        supportStock.text = support
+                        val autre = dialog.stock_autre.text
+                        autre_acc_stock.text = autre
+                        stock.stockAccessoir.cable = cable.toString().toInt()
+                        model.addStock(stock)
+                        Toast.makeText(applicationContext, "stock change", Toast.LENGTH_LONG).show()
+                    }
+                    1 -> {
+                    }
+                    2 -> {
+                    }
+                    3 -> {
+                    }
+                }
+
+
+                dialog.dismiss()
+            }
+            dialog.negativeButton {
+                Toast.makeText(baseContext, "cancel", Toast.LENGTH_LONG).show()
+                dialog.dismiss()
+            }
+            dialog.show()
         }
+        model.fitchClients()
+        model.getRealtimeUpdates()
+        model.stocks.observe(this, Observer {
+            val stockAcc = StockAccessoir()
+            stock_accessoir_cable.text = it.stockAccessoir.cable.toString()
+            it.stockAccessoir = stockAcc
+            stock.stockAccessoir = stockAcc
+
+        })
+
+
     }
 }
--- app/src/main/java/com/issamelasri/adminappretail/stock/ViewModelStock.kt
+++ app/src/main/java/com/issamelasri/adminappretail/stock/ViewModelStock.kt
@@ -4,14 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.issamelasri.adminappretail.STOCK_NODE
 import com.issamelasri.adminappretail.stock.models.StockGlobal
 
 class ViewModelStock : ViewModel() {
 
     private var dbClients = FirebaseDatabase.getInstance().getReference(STOCK_NODE)
-    lateinit var auth: FirebaseAuth
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val email = auth.currentUser?.email?.replace("@", "")
+        ?.replace(".", "")
 
     private val _result = MutableLiveData<Exception?>()
     val result: LiveData<Exception?>
@@ -19,6 +21,80 @@ class ViewModelStock : ViewModel() {
     private val _stocks = MutableLiveData<StockGlobal>()
     val stocks: LiveData<StockGlobal>
         get() = _stocks
+
+    private val childEventListener = object : ChildEventListener {
+        override fun onCancelled(error: DatabaseError) {}
+        override fun onChildMoved(snapshot: DataSnapshot, p1: String?) {}
+        override fun onChildChanged(snapshot: DataSnapshot, p1: String?) {
+            val stockVendeur = snapshot.getValue(StockGlobal::class.java)
+            stockVendeur?.id = snapshot.key.toString()
+            _stocks.value = stockVendeur
+        }
+
+        override fun onChildAdded(snapshot: DataSnapshot, p1: String?) {
+            val stockVendeur = snapshot.getValue(StockGlobal::class.java)
+            stockVendeur?.id = snapshot.key.toString()
+            _stocks.value = stockVendeur
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+            val stockVendeur = snapshot.getValue(StockGlobal::class.java)
+            stockVendeur?.id = snapshot.key.toString()
+            stockVendeur?.isDeleted = true
+            _stocks.value = stockVendeur
+        }
+    }
+
+    fun getRealtimeUpdates() {
+        dbClients.addChildEventListener(childEventListener)
+    }
+
+    fun fitchClients() {
+        dbClients.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(erreur: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val stocks = StockGlobal()
+                    for (clientSnapshot in snapshot.children) {
+                        val client = clientSnapshot.getValue(StockGlobal::class.java)
+                        client?.id = clientSnapshot.key.toString()
+                        client?.let {
+                            stocks.recharge = it.recharge
+                            stocks.stockAccessoir = it.stockAccessoir
+                            stocks.stockPhones = it.stockPhones
+                            stocks.stockSim = it.stockSim
+                        }
+                    }
+                    _stocks.value = stocks
+                }
+            }
+
+        })
+    }
+
+    fun addStock(stock: StockGlobal) {
+        dbClients = FirebaseDatabase.getInstance().getReference(STOCK_NODE)
+        this.email?.let { it ->
+            dbClients.child(it).setValue(stock)
+                .addOnCompleteListener {
+                    if (
+                        it.isSuccessful) {
+                        _result.value = null
+                    } else {
+                        _result.value = it.exception
+                    }
+                }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dbClients.removeEventListener(childEventListener)
+    }
+
+
 }
 
 
